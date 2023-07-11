2eaf51e291f6f97c0dde4791f3b087fab409a659
tranhieugif <thanhhieutrandinh@gmail.com>
2023-07-11 15:11:27 +0700

    Cải thiện navbar

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ $(document).ready(function () {
     dots: true,
   });
 });
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+    console.log(window.scrollY);
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
