17495027ba633d568f71ae56e1ec71e28ed96fbd
tranhieugif <thanhhieutrandinh@gmail.com>
2023-08-04 16:20:43 +0700

    Làm nút zalo và cải thiện phần navbar

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  $(".food-menu-list").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: true,
-    arrows: true,
-    draggable: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-    cssEase: "linear",
-    prevArrow: `<button type='button' class='slick-prev slick-arrow ti-angle-left'></button>`,
-    nextArrow: `<button type='button' class='slick-next slick-arrow ti-angle-right'></button>`,
-    dots: true,
-  });
+    $(".food-menu-list").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        draggable: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        cssEase: "linear",
+        prevArrow: `<button type='button' class='slick-prev slick-arrow ti-angle-left'></button>`,
+        nextArrow: `<button type='button' class='slick-next slick-arrow ti-angle-right'></button>`,
+        dots: true,
+    });
 });
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
-  myFunction();
+    myFunction();
 };
 
 // Get the navbar
@@ -27,9 +27,12 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
+$(window).load(function () {
+    $("body").removeClass("preload");
+});
