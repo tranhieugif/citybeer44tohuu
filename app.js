7b23d3b77118656e1fa3f53b319ce07d8808a507
tranhieugif <thanhhieutrandinh@gmail.com>
2023-08-26 16:42:28 +0700

    Làm section customer-review và Lightbox gallery

--- app.js
+++ app.js
@@ -28,6 +28,43 @@ $(document).ready(function () {
         ],
     });
 });
+// handle customer review slider
+$(document).ready(function () {
+    $(".customer-reviews .feedback-body").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: `<button type='button' class='slick-prev slick-arrow ti-angle-left'></button>`,
+        nextArrow: `<button type='button' class='slick-next slick-arrow ti-angle-right'></button>`,
+    });
+});
+// Lightbox gallery
+$(document).ready(function () {
+    $(".img-album").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function (element) {
+                return element.find("img");
+            },
+        },
+    });
+});
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
     myFunction();
